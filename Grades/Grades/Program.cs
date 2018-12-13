186fbe2d6c562eef3524bee08b308e755f0d627d
zebby <zebbykhairah@gmail.com>
2018-12-13 09:26:20 +0800

    #6: Methods: #1

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -11,16 +11,37 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-            synth.Speak("hello"); 
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+            //synth.Speak("hello");
 
             GradeBook book =  new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.HighestGrade);
 
+            WriteResult("Avg grade is", stats.AverageGrade,2,3,4,5);
+            WriteResult("Highest grade is", (int)stats.HighestGrade);
+
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, params float[] result)
+        {
+            foreach (float res in result)
+            {
+                Console.WriteLine(description + ": " + res);
+            }
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
         }
     }
 }
